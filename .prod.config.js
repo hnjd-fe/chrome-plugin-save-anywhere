b7bbd445718c6c6346b3c69794532a5a243a64c8
qiushaowei <qiushaowei@qianxin.com>
2019-07-08 09:27:22 +0800

    up

--- .prod.config.js
+++ .prod.config.js
@@ -1,7 +1,7 @@
 // 不需要的字段就注释掉！
 module.exports = {
-    host: 'localhost.',
-    port: 9311,
+    //host: 'localhost.',
+    //port: 9311,
     // branch: ''
     // targetDir: 'public',
     f2e_version: new Date().getTime(),
