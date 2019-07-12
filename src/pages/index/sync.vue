446e9b9c7f6075647981023a66816c833eec67ea
qiushaowei <qiushaowei@qianxin.com>
2019-07-13 00:17:24 +0800

    up

--- src/pages/index/sync.vue
+++ src/pages/index/sync.vue
@@ -7,6 +7,9 @@
         <el-row>
             <el-button type="primary" @click="login" style="width:180px;">
                 {{$t('login_github')}}<i class="el-icon-user-solid el-icon--right"></i></el-button>
+
+                <br />
+                <iframe :src="iframeUrl" width="80%" height="500" @load="iframeOnload" ref="iframe"></iframe>
         </el-row>
     </el-main>
 </el-container>
@@ -25,19 +28,32 @@ import dataMixin from '@src/mixin/data.js'
 
 const packInfo = require( '@root/package.json' )
 
+window.addEventListener('message',function(e){
+    let data =e.data;
+    console.log( 'receive iframe data', data );
+    console.dir( e );
+},false);
+
 export default {
     mixins: [ dataMixin ]
     , data() {
         return {
+            iframeUrl: 'about:blank'
         }
     }
     , methods: {
         login() {
             let backUrl = encodeURIComponent( location.href );
-            let oauth = `http://btbtd.org/api/saveanywhere?backurl=${backUrl}&logintype=github`
+            let oauth = `http://btbtd.org/api/saveanywhere?logintype=github`
+            oauth = 'http://btbtd.org/api/saveanywhere/?s=/Index/Index/pushmessage';
 
             console.log( oauth );
-            location.href = oauth;
+            //location.href = oauth;
+            this.iframeUrl = oauth;
+        }
+        , iframeOnload(){
+            console.log( 'onload', Date.now() );
+            console.dir( this.$refs.iframe );
         }
     }
     , mounted(){
